404cab8cd0f725f11938568be407d33988465e44
omacorr <o.correa@alumno.etec.um.edu.ar>
2025-10-24 10:17:51 -0300

    Clase Match

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,5 @@
 package org.example
 
-import kotlin.math.roundToInt
-
 fun main(){
 
 }
--- app/src/main/kotlin/org/example/Team.kt
+++ app/src/main/kotlin/org/example/Team.kt
@@ -12,7 +12,6 @@ class Team(val name: String) {
     fun seleccJugador(jugador: Player) {
         if (jugadores.size < 5) {
             jugadores.add(jugador)
-            println("${jugador.nombre} fichado correctamente para $name")
         } else {
             println("No se puede seleccionar más jugadores. Límite de 5 jugadores alcanzado para ${name}")
         }
@@ -42,13 +41,4 @@ class Team(val name: String) {
         return "$plantilla"
     }
 
-    fun equipoSisitante(nombre: String): Team{
-        val equipo = Team(nombre)
-        equipo.seleccJugador(Player(16, "Pancho", 1.95, PlayerPosition.PIVOT, PlayerSkills(80, 87, 85, 92, 95)))
-        equipo.seleccJugador(Player(19, "Checho", 1.80, PlayerPosition.ALA_PIVOT, PlayerSkills(80, 57, 60, 62, 70)))
-        equipo.seleccJugador(Player(16, "juanb", 1.95, PlayerPosition.ALERO, PlayerSkills(62, 73, 73, 52, 65)))
-        equipo.seleccJugador(Player(15, "rami", 1.80, PlayerPosition.ESCOLTA, PlayerSkills(90, 80, 78, 75, 80)))
-        equipo.seleccJugador(Player(16, "yo", 1.80, PlayerPosition.BASE, PlayerSkills(86, 77, 80, 62, 80)))
-        return equipo
-    }
 }
